9551b27ab56f40cd8f6ea1c3ab32d55be43ba876
Cloud Technet <69082631+cloudtechnet@users.noreply.github.com>
2026-07-09 10:06:40 +0530

    second update

--- src/main/java/com/cloudtechnet/App.java
+++ src/main/java/com/cloudtechnet/App.java
@@ -3,7 +3,7 @@ package com.cloudtechnet;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to GitHub Actions Cache Demo");
+        System.out.println("Cache Demo");
     }
 
     public static int add(int a, int b) {
